86798e7d53bb0756a43537a00af0691b92510bfe
GABINMOON <105955232+GABINMOON@users.noreply.github.com>
2022-05-24 06:59:38 +0900

    Gabin p5 files

---
+++ P5/p5/empty-example/sketch.js
@@ -0,0 +1,42 @@
+
+
+// for red, green, and blue color values
+let r, g, b;
+let r1,g1,b1
+
+function setup() {
+  createCanvas(1450, 900);
+  // Pick colors randomly
+  r = random(255);
+  g = random(255);
+  b = random(255);
+
+  r1 = random(255);
+  g1= random(255);
+  b1 = random(255);
+}
+
+function draw() {
+  background(r1,b1,g1,255);
+  // Draw a circle
+  strokeWeight(2);
+  noStroke();
+  fill(r, g, b, 255);
+  ellipse(360, 200, 200, 200);
+}
+
+// When the user clicks the mouse
+function mousePressed() {
+  // Check if mouse is inside the circle
+  let d = dist(mouseX, mouseY, 360, 200);
+  if (d < 100) {
+    // Pick new random color values
+    r = random(255);
+    g = random(255);
+    b = random(255);
+
+    r1 = random(255);
+  g1= random(255);
+  b1 = random(255);
+  }
+}
